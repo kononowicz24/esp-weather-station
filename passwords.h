f6a949332028c7956fb00ebb1362be677723164e
Krzysztof Kondratowicz <kononowicz24@users.noreply.github.com>
2017-11-23 23:55:46 +0100

    Update passwords.h

--- passwords.h
+++ passwords.h
@@ -3,7 +3,7 @@
 #ifndef __PASSWORDS_H__
 #define __PASSWORDS_H__
 
-const char *ssid = "UPC247602010";
-const char *password = "HFLAZPSY";
+const char *ssid = "YOUR SSID";
+const char *password = "YOUR PASS";
 
 #endif
